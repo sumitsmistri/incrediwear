50ce4931ce0b2f06526d102df3cfe2b9c1b6329a
Yousuf Jamal <mistri.sumit21@gmail.com>
2023-04-15 13:38:18 +0400

    new commit

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,8 @@ export default function App({ Component, pageProps }) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
       </Head>
-      <AppInside Component={Component} pageProps={pageProps} />
+      {/* <AppInside Component={Component} pageProps={pageProps} /> */}
+      <Component {...pageProps} />
     </Provider>
   );
 }
