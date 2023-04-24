b16e4d30ebb09ad55a197669b22be9eab928bd31
Yousuf Jamal <mistri.sumit21@gmail.com>
2023-04-25 00:55:14 +0400

    saving data in google sheet with googleapis and 02 slide

--- pages/api/sheet.js
+++ pages/api/sheet.js
@@ -39,7 +39,6 @@ async function handler(req, res) {
 
         res.status(201).json({ message: 'It works!', response });
     }
-    res.status(200).json({ message: 'Hey!' });
 }
 
 export default handler;
